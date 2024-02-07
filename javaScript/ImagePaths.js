7ec5f33de759849471938e719d687a96f06aaec8
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-07 11:05:14 +0530

    Remove unused ImagePaths.jsx and add image preloading in index.html

---
+++ javaScript/ImagePaths.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+var imagePaths = [
+  "images/hero-image.jpg",
+  "images/feature1-image.jpg",
+  "images/feature2-image.jpg",
+  "images/feature3-image.jpg",
+  "images/step1-image.jpg",
+  "images/step2-image.jpg",
+  "images/step3-image.jpg",
+  "images/testimonial1-image.jpg",
+  "images/testimonial2-image.jpg",
+  "images/cta-image.jpg",
+];
